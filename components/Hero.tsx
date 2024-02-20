ab5a9e0577dcc57c4077260d64e2e3afa6db10d9
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2024-02-20 14:30:11 +0100

    component Hero set responsive

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div id="hero" className='h-[30rem] flex flex-col items-center justify-center'>
-        <p className='text-4xl text-center'>Bienvenus sur le portfolio de </p>
+    <div id="hero" className='h-full sm:h-[30rem] flex flex-col items-center justify-center py-2'>
+        <p className='text-4xl text-center'>Bienvenue sur le portfolio de </p>
         <h1 className='text-6xl text-center font-bold p-4'>Jean-Jacques Valmont</h1>
-        <p className='w-1/2 text-4xl text-center'> Développeur front-end en recherche d&apos;alternance pour mars 2024 sur Paris</p>
+        <p className='w-full sm:w-1/2 text-4xl text-center'> Développeur front-end React en recherche d&apos;alternance pour mars 2024 sur Paris</p>
     </div>
   )
 }

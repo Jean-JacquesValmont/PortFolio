ca58e35903cc97fc91f299888e82089415186ce8
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2024-02-20 17:02:44 +0100

    Edited component About with a accordion

--- components/About.tsx
+++ components/About.tsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
+import AccordionAbout from './AccordionAbout'
 
 const About = () => {
   return (
     <div id="about" className='flex flex-col items-center justify-center py-2'>
         <h2 className='text-6xl pb-4'>Présentation</h2>
-        <div className='flex flex-col lg:flex-row'>
-            <div className='flex basis-1/3 justify-center'>
+        <div className='w-full flex flex-col lg:flex-row items-center'>
+            <div className='w-1/2 flex justify-end md:justify-center'>
                 <Image src="/image/Photo_de_profil_Jean-Jacques_Valmont.jpg" alt="Photo_de_profil_Jean-Jacques_Valmont" width={400} height={400}/>  
             </div>
-            <div className='w-full lg:w-[64rem] flex flex-col gap-4 basis-2/3 p-4 lg px-4'>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim voluptatum at libero facilis, modi quaerat corporis quos quas dolorem ratione dolores minima repellat nulla ab sed eius inventore sint numquam.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim voluptatum at libero facilis, modi quaerat corporis quos quas dolorem ratione dolores minima repellat nulla ab sed eius inventore sint numquam.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim voluptatum at libero facilis, modi quaerat corporis quos quas dolorem ratione dolores minima repellat nulla ab sed eius inventore sint numquam.</p>
+            <div className='w-full flex items-center justify-start md:justify-center pr-2'>
+              <AccordionAbout />
             </div>
         </div>
     </div>

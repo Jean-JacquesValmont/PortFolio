17b0177dbaadb2d268bcc0ed57131e2906d73c8c
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2024-02-20 14:38:36 +0100

    component About set responsive

--- components/About.tsx
+++ components/About.tsx
@@ -5,11 +5,11 @@ const About = () => {
   return (
     <div id="about" className='flex flex-col items-center justify-center py-2'>
         <h2 className='text-6xl pb-4'>Présentation</h2>
-        <div className='flex'>
+        <div className='flex flex-col lg:flex-row'>
             <div className='flex basis-1/3 justify-center'>
                 <Image src="/image/Photo_de_profil_Jean-Jacques_Valmont.jpg" alt="Photo_de_profil_Jean-Jacques_Valmont" width={400} height={400}/>  
             </div>
-            <div className='w-[64rem] flex flex-col gap-4 basis-2/3'>
+            <div className='w-full lg:w-[64rem] flex flex-col gap-4 basis-2/3 p-4 lg px-4'>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim voluptatum at libero facilis, modi quaerat corporis quos quas dolorem ratione dolores minima repellat nulla ab sed eius inventore sint numquam.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim voluptatum at libero facilis, modi quaerat corporis quos quas dolorem ratione dolores minima repellat nulla ab sed eius inventore sint numquam.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim voluptatum at libero facilis, modi quaerat corporis quos quas dolorem ratione dolores minima repellat nulla ab sed eius inventore sint numquam.</p>

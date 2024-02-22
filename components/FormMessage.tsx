2ffd247afae4a0d4ae685658002ceaac0bc75d7b
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2024-02-22 15:50:02 +0100

    Implementation of FormSubmit to send emails to the form.

--- components/FormMessage.tsx
+++ components/FormMessage.tsx
@@ -10,28 +10,33 @@ const FormMessage = () => {
 
     const onSubmit = (data : any) => {
         console.log(data);
+        // onSubmit={handleSubmit(onSubmit)}
     };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form action="https://formsubmit.co/bc4bbd2b00c779c8ea12f752a8e1a2fb"  method="POST">
         <div className='flex flex-col items-center justify-center pb-4'>
             <div className='flex w-full sm:w-[33rem]'>
                 <div className='w-full sm:w-[33rem] pb-4 px-2'>
-                    <p>Prénom</p>
-                    <Input {...register("firstName")} type="text" placeholder="Prénom"/>
+                    <p>Prénom*</p>
+                    <Input {...register("firstName")} type="text" placeholder="Prénom" name="firstName" required/>
                 </div>
                 <div className='w-full sm:w-[33rem] pb-4 px-2'>
-                    <p>Nom</p>
-                    <Input {...register("lastName")} type="text" placeholder="Nom"/>
+                    <p>Nom*</p>
+                    <Input {...register("lastName")} type="text" placeholder="Nom" name="lastName" required/>
                 </div>
             </div>
             <div className='w-full sm:w-[33rem] p-2 sm:pb-4'>
-                <p>Email</p>
-                <Input {...register("email")} type="email" placeholder="Email"/>
+                <p>Email*</p>
+                <Input {...register("email")} type="email" placeholder="Email" name="email" required/>
             </div>
             <div className='w-full sm:w-[33rem] p-2 sm:pb-4'>
-                <p>Votre message</p>
-                <Textarea {...register("message")} placeholder="Ecrivez votre message ici." />
+                <p>Sujet*</p>
+                <Input {...register("subject")} type="text" placeholder="Suject" name="subject" required/>
+            </div>
+            <div className='w-full sm:w-[33rem] p-2 sm:pb-4'>
+                <p>Votre message*</p>
+                <Textarea {...register("message")} placeholder="Ecrivez votre message ici." name="message" required/>
             </div>
             <Button className="hover:bg-black hover:text-white" variant="outline" type="submit">Envoyer</Button>
         </div>

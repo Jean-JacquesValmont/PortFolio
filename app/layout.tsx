a2a70405a67bdba6acbfdc9ce9520323bc6f4ef6
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2024-06-12 10:14:38 +0200

    Updated Hero for month search alternance.

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,13 +10,13 @@ const openSans = Open_Sans({ subsets: ["latin"]});
 export const metadata: Metadata = {
   metadataBase: new URL('https://port-folio-three-umber.vercel.app/'),
   title: "Portfolio Jean-Jacques Valmont",
-  description: "Portfolio de Jean-Jacques Valmont. Développeur front-end React en recherche d'alternance pour mars 2024 sur Paris",
+  description: "Portfolio de Jean-Jacques Valmont. Développeur front-end React en recherche d'alternance pour septembre 2024 sur Paris",
   creator: 'Jean-Jacques Valmont',
   openGraph: {
     type: 'website',
     url: 'https://port-folio-three-umber.vercel.app/',
     title: 'Portfolio Jean-Jacques Valmont',
-    description: 'Portfolio de Jean-Jacques Valmont. Développeur front-end React en recherche d\'alternance pour mars 2024 sur Paris',
+    description: 'Portfolio de Jean-Jacques Valmont. Développeur front-end React en recherche d\'alternance pour septembre 2024 sur Paris',
     images: [
       {
         url: "/image/Portfolio_Jean-Jacques_Valmont.png",
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <div id="hero" className="h-full sm:h-[30rem] flex flex-col items-center justify-center py-2 transition-opacity">
         <p className='text-2xl sm:text-4xl text-center px-4'>Bienvenue sur le portfolio de </p>
         <h1 className='text-2xl sm:text-6xl text-center font-bold p-4'>Jean-Jacques Valmont</h1>
-        <p className='w-full sm:w-1/2 text-2xl sm:text-4xl text-center px-4'> Développeur front-end React en recherche d&apos;alternance pour avril 2024 sur Paris</p>
+        <p className='w-full sm:w-1/2 text-2xl sm:text-4xl text-center px-4'> Développeur front-end React en recherche d&apos;alternance pour septembre 2024 sur Paris</p>
     </div>
   )
 }
